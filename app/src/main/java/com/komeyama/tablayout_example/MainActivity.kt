96de30393c01fbab7a4a20618cd54168f115e090
YoneyamaShunpei <yoneyamashunpei@gmail.com>
2020-10-15 00:09:30 +0900

    add: timber

--- app/src/main/java/com/komeyama/tablayout_example/MainActivity.kt
+++ app/src/main/java/com/komeyama/tablayout_example/MainActivity.kt
@@ -8,11 +8,13 @@ import com.xwray.groupie.GroupieViewHolder
 import com.xwray.groupie.Item
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.item_simple.view.*
+import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Timber.plant(Timber.DebugTree())
 
         val adapter = GroupAdapter<GroupieViewHolder>()
         for (i in 1..20) {
@@ -29,7 +31,9 @@ class MainActivity : AppCompatActivity() {
 
             override fun onTabUnselected(p0: TabLayout.Tab?) {}
 
-            override fun onTabSelected(p0: TabLayout.Tab?) {}
+            override fun onTabSelected(p0: TabLayout.Tab?) {
+                Timber.d("on tab selected %s", p0)
+            }
         })
     }
 }
